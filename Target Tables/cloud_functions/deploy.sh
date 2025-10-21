ecf68d5e5555ffc983044defdd6726a952f250fa
heyucheng23 <112510969+heyucheng23@users.noreply.github.com>
2025-10-21 17:17:19 -0400

    Update deploy.sh

--- Target Tables/cloud_functions/deploy.sh
+++ Target Tables/cloud_functions/deploy.sh
@@ -4,6 +4,15 @@ SA_EMAIL=$(gcloud functions describe load_api_to_motherduck --region=us-central1
 for secret in project_motherduck_token target_api_test_amisha; do   gcloud secrets add-iam-policy-binding $secret     --member="serviceAccount:${SA_EMAIL}"     --role="roles/secretmanager.secretAccessor"; done
 
 -- Deployment
+
+gcloud run deploy nearby-stores \
+  --image "us-central1-docker.pkg.dev/ba882-team4/store-loader/nearby-stores:rev-$(date +%Y%m%d-%H%M%S)" \
+  --region us-central1 \
+  --platform managed \
+  --allow-unauthenticated \
+  --set-secrets MOTHERDUCK_TOKEN=project_motherduck_token:latest,RAPIDAPI_KEY=rapidapi_target_key:latest \
+  --update-env-vars MD_CATALOG=project_882
+
 gcloud functions deploy product-search-api-to-duckdb   \
         --gen2   --runtime=python311   \
         --region=us-central1   --source=.  \
